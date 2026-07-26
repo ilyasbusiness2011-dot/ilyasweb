4ffc0fbd0639766e2eea70b108d50dcb10ae9e01
ilyasbusiness2011-dot <ilyasbusiness2011@gmail.com>
2026-07-26 14:54:52 +0100

    Initialize Supabase client in script.js

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
+import { createClient } from 'https://esm.sh/@supabase/supabase-js'
 
+const supabaseUrl = 'https://heolqzakdwsvlhhrtdjn.supabase.co/rest/v1/'
+const supabaseKey = 'sb_publishable_9QsBYrVvfUEcUXHp96sgYQ_w8tWm7vi'
+
+const supabase = createClient(supabaseUrl, supabaseKey)
+
+console.log('Supabase connected!')
